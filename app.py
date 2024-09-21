d250487572094e528ef356d9f530ae9bdd5a8233
Sritan Motati <sritanmotati@gmail.com>
2024-09-21 15:08:04 -0400

    tune studio

--- app.py
+++ app.py
@@ -1,18 +1,14 @@
 from flask import Flask
 from transformers import pipeline
 import os
-from cerebras.cloud.sdk import Cerebras
-from dotenv import load_dotenv
+import requests
+# from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
 
 app = Flask(__name__)
 
 sentiment_pipeline = pipeline("sentiment-analysis")
-
-client = Cerebras(
-    api_key=os.environ.get("CEREBRAS_API_KEY"),
-)
 
 @app.route('/')
 def hello_world():
@@ -24,16 +20,30 @@ def sentiment_analysis(text):
 
 @app.route('/cerebras/prompt=<prompt>')
 def cerebras(prompt):
-    chat_completion = client.chat.completions.create(
-        messages=[
-            {
-                "role": "user",
-                "content": prompt,
-            }
+    url = "https://proxy.tune.app/chat/completions"
+    headers = {
+        "Authorization": "sk-tune-LWw4p6Gu8psAwdpVfL4eLkwLtNctxp416KL",
+        "Content-Type": "application/json",
+    }
+    data = {
+    "temperature": 0.9,
+        "messages":  [
+        {
+            "role": "system",
+            "content": "You are an expert on maximizing small businesses. You will be given metrics and reviews and such data. Sometimes you will be asked to give recommendations for the business to improve, other times you will simply summarize what you're given."
+        },
+        {
+            "role": "user",
+            "content": prompt,
+        }
         ],
-        model="llama3.1-8b",
-    )
-    return chat_completion.choices[0].message.content
+        "model": "meta/llama-3.1-8b-instruct",
+        "stream": False,
+        # "frequency_penalty":  0.2,
+        # "max_tokens": 100
+    }
+    response = requests.post(url, headers=headers, json=data)
+    return response.json()["choices"][0]["message"]["content"]
 
 if __name__ == '__main__':
-    app.run(debug=True, port=3000)
+    app.run(debug=True, port=5000)

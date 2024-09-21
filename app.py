964c8fe3d927f397442c000caff8c685061f3993
Sritan Motati <sritanmotati@gmail.com>
2024-09-21 04:27:29 -0400

    fixed cerebras

--- app.py
+++ app.py
@@ -1,21 +1,22 @@
-
-# A very simple Flask Hello World app for you to get started with...
-
 from flask import Flask
 from transformers import pipeline
 import os
 from cerebras.cloud.sdk import Cerebras
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
 sentiment_pipeline = pipeline("sentiment-analysis")
+
 client = Cerebras(
     api_key=os.environ.get("CEREBRAS_API_KEY"),
 )
 
 @app.route('/')
 def hello_world():
-    return 'Hello from Flask!'
+    return 'Hey from Flask!'
 
 @app.route('/sentiment/text=<text>')
 def sentiment_analysis(text):
@@ -32,7 +33,7 @@ def cerebras(prompt):
         ],
         model="llama3.1-8b",
     )
-    return chat_completion
+    return chat_completion.choices[0].message.content
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, port=3000)

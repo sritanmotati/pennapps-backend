45250e69cd476873294bc85c1a393f7893ddecff
Sritan Motati <sritanmotati@gmail.com>
2024-09-21 04:14:32 -0400

    added cerebras

--- app.py
+++ app.py
@@ -3,10 +3,15 @@
 
 from flask import Flask
 from transformers import pipeline
+import os
+from cerebras.cloud.sdk import Cerebras
 
 app = Flask(__name__)
 
 sentiment_pipeline = pipeline("sentiment-analysis")
+client = Cerebras(
+    api_key=os.environ.get("CEREBRAS_API_KEY"),
+)
 
 @app.route('/')
 def hello_world():
@@ -15,6 +20,19 @@ def hello_world():
 @app.route('/sentiment/text=<text>')
 def sentiment_analysis(text):
     return sentiment_pipeline(text)
+
+@app.route('/cerebras/prompt=<prompt>')
+def cerebras(prompt):
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": prompt,
+            }
+        ],
+        model="llama3.1-8b",
+    )
+    return chat_completion
 
 if __name__ == '__main__':
     app.run()

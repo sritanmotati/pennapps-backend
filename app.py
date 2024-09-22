ee200b7e7159dab3da84861c04db317912d8db1d
Sritan Motati <sritanmotati@gmail.com>
2024-09-22 00:39:37 -0400

    code changes

--- app.py
+++ app.py
@@ -18,6 +18,33 @@ def hello_world():
 def sentiment_analysis(text):
     return sentiment_pipeline(text)
 
+@app.route('/topic/prompt=<prompt>')
+def topic_classifier(prompt):
+    url = "https://proxy.tune.app/chat/completions"
+    headers = {
+        "Authorization": "sk-tune-LWw4p6Gu8psAwdpVfL4eLkwLtNctxp416KL",
+        "Content-Type": "application/json",
+    }
+    data = {
+    "temperature": 0.9,
+        "messages":  [
+        {
+            "role": "system",
+            "content": "You will be given text from reviews on a restaurant. Classify the main focus of the review as either food, service, or environment. Only provide the one word answer."
+        },
+        {
+            "role": "user",
+            "content": prompt,
+        }
+        ],
+        "model": "meta/llama-3.1-8b-instruct",
+        "stream": False,
+        # "frequency_penalty":  0.2,
+        "max_tokens": 4
+    }
+    response = requests.post(url, headers=headers, json=data)
+    return response.json()["choices"][0]["message"]["content"]
+
 @app.route('/cerebras/prompt=<prompt>')
 def cerebras(prompt):
     url = "https://proxy.tune.app/chat/completions"
